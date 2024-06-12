d06f7e861ec1433c0d462c9425e20065faf07c1f
Ben Harper <benjaminitonio@gmail.com>
2024-06-12 14:11:35 -0500

    Dart - Asynchronous Task 3. Greet User v1

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,25 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String username = await fetchUserData();
+    Map<String, dynamic> userMap = jsonDecode(username);
+    return "Hello ${userMap['username']}";
+  } catch (err) {
+    print('error caught: $err');
+  }
+
+  return '';
+}
+
+Future<String> loginUser() async {
+  bool verified = await checkCredentials();
+  if (verified){
+    print('There is a user: true');
+  } else {
+    print('There is a user: true');
+    return 'Wrong Credentials';
+  }
+  return greetUser();
+}
